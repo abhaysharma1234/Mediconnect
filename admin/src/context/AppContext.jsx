8ff85722c80f34b6610221c6c170f56a8015e859
abhaysharma1234 <122429979+abhaysharma1234@users.noreply.github.com>
2025-05-26 23:50:41 +0530

    Update AppContext.jsx

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -6,7 +6,7 @@ export const AppContext = createContext()
 const AppContextProvider = (props) => {
 
     const currency = import.meta.env.VITE_CURRENCY
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "https://mediconnect-backend-awvu.onrender.com"
     //const comission = 0
 
     const months = [" ","Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
